f5dd61767ab2bd3fd88d9dc0f52994a6d4dd1299
민진 <whxorb44@gmail.com>
2020-07-20 17:25:27 +0900

    java commit test

---
+++ test_1/src/ssafy/com/Test1.java
@@ -0,0 +1,12 @@
+package ssafy.com;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		boolean flg = false;
+		
+		int result = (flg == true) ? 1:2;
+		System.out.println(result);
+	}
+
+}
